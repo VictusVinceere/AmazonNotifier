4203d7b1fa2dabf97758502c7a9ce8cfa713b750
Vvicii <vinceere@gmail.com>
2019-05-27 00:16:54 +0500

    initial commit

---
+++ src/main/java/com/notificsound/App.java
@@ -0,0 +1,5 @@
+package com.notificsound;
+
+public class App {
+
+}
---
+++ src/test/java/com/notificsound/AppTest.java
@@ -0,0 +1,12 @@
+package com.notificsound;
+
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+	}
+
+}
